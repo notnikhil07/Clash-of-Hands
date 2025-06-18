c2b058bb04c21e7c104daa3d48be43a373d73466
Nikhil Kumar <vkum9209@gmail.com>
2025-06-19 00:08:57 +0530

    hand clash

---
+++ index.js
@@ -0,0 +1,84 @@
+let userScore = 0;
+let comScore = 0;
+
+
+const userScorePara = document.querySelector("#user_score");
+const comScorePara = document.querySelector("#comp_score");
+const compChoices = document.querySelectorAll(".comp_choices")
+const choices = document.querySelectorAll(".choice");
+const pickBtn = document.querySelector(".select_button");
+let selectedCompChoiceIndex = -1;
+let selectedChoiceIndex = -1;
+const showWinner = (UserWin) => {
+    if (UserWin) {
+        console.log("S: ",selectedChoiceIndex)
+        userScore++;
+        userScorePara.innerText = userScore;
+        pickBtn.innerText = "You Win!";
+        pickBtn.style.backgroundColor = "Green";
+       
+    } else {
+        comScore++;
+        comScorePara.innerText = comScore;
+        pickBtn.innerHTML = "You Loose!";
+        pickBtn.style.backgroundColor = "Red";
+    }
+};
+
+    const options = [1,2,3]
+    let comIndex = -1;
+
+const genCompChoice = () => {
+    const randIdx = Math.floor(Math.random() * 3);
+    compChoices[randIdx].style.transform = "translatex(100px)";
+    setTimeout(()=>{
+            compChoices[randIdx].style.transform = "translatex(0px)";
+
+         }, 500)
+    comIndex = randIdx;
+    return options[randIdx];
+}
+
+
+
+const playGame = (userChoice) =>{
+    console.log("User choice = ", userChoice)
+    const compChoice = genCompChoice();
+    console.log("comp choice = ",compChoice);
+    if(userChoice == compChoice){
+        pickBtn.innerText = "Game Draw";
+        pickBtn.style.backgroundColor = "grey";
+
+    }
+    else{
+        let UserWin = true;
+        if(userChoice == "1"){
+            UserWin = compChoice == "2" ? false : true;
+        }
+        else if(userChoice == "2"){
+            UserWin = compChoice == "3" ? false : true;
+        }
+        else if(userChoice == "3"){
+            UserWin = compChoice == "1" ? false : true;
+            }
+        showWinner(UserWin);
+    }
+};
+
+
+
+choices.forEach((choice, index) =>{
+    // console.log(choice);
+    choice.addEventListener("click",() =>{
+    
+         choice.style.transform = "translatex(-100px)";
+         setTimeout(()=>{
+            choice.style.transform = "translatex(0px)";
+
+         }, 500)
+        // console.log("SELECTED CHOICE", index)
+        selectedChoiceIndex = index
+        const userChoice = choice.getAttribute("value");
+        playGame(userChoice);
+    })
+})
